68c95f9060e6629e785271e6bbac28699b4fafa7
vovuk51 <vovuk51@mail.ru>
2022-09-23 16:23:42 +0300

    site header
    Заглушка на шапку сайта

--- Smotrobitva/index.php
+++ Smotrobitva/index.php
@@ -4,6 +4,45 @@ require 'dbcon.php';
 
 include('includes/header.php') ?>
 
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">
+        <img src="../assets/brand.svg" width="40" height="40" alt="WatchButtle">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Аниме</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Манга</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Персонажи</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Поиск</button>
+        </form>
+        <div class="dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="../assets/simpleUser.svg" width="40" height="40" border-radius: 50% alt="Фото пользователя"> Меню
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Действие 1</a></li>
+              <li><a class="dropdown-item" href="#">Действие 2</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Ещё действие</a></li>
+            </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+
 <div class="container mt-5">
     <h1>
         <p class="text-center fs-1"><strong>Смотробитва</strong></p>    
